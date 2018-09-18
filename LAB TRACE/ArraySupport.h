c6e09914b52d8c016c7b1aadd6e08ea3a6859249
teknically <42884522+teknically@users.noreply.github.com>
2018-09-17 18:46:57 -0700

    Add files via upload

---
+++ LAB TRACE/ArraySupport.h
@@ -0,0 +1,10 @@
+#ifndef arraySupport_h
+#define arraySupport_h
+
+// A function to get the value at a specific position
+int getIndex(int* list, int pos){
+    // Provide your code here...
+	return list[pos];
+	
+}
+#endif
